b4cc7a177010aa2d065979f07a3330c145d663b5
Pravin L <75366950+Pravinlsiye@users.noreply.github.com>
2022-05-17 17:08:32 +0530

    Update Auto email Sender.py

--- Auto email Sender.py
+++ Auto email Sender.py
@@ -22,21 +22,19 @@ MY_ADDRESS = ‘**********@gmail.com’
   next(csv_reader)
   for lines in csv_reader:
    msg = MIMEMultipart() # create a message
-# add in the actual person name to the message template
+
     message =    message_template.substitute(PERSON_NAME=row[0],MATH=row[2],
     ENG=row[3],SCI=row[4])
    print(message)
-# setup the parameters of the message
+
    msg[‘From’]=MY_ADDRESS
    msg[‘To’]=lines[1]
    msg[‘Subject’]=”Mid term grades”
-# add in the message body
+
    msg.attach(MIMEText(message, ‘plain’))
-# send the message via the server set up earlier.
+
    s.send_message(msg)
    del msg
- 
- # Terminate the SMTP session and close the connection
  s.quit()
 if __name__ == ‘__main__’:
  main()
